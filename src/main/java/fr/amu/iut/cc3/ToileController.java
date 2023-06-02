dfd6e4b4e2eba917291064c1ce668f14f699bf4c
Maxime TAMARIN <maxime.tamarin@etu.univ-amu.fr>
2023-06-02 16:03:41 +0200

    Partie 2 fonctionnelle

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -30,6 +31,12 @@ public class ToileController implements Initializable {
     private static int angleEnDegre = 60;
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
+
+    @FXML
+    HBox scene;
+
+    @FXML
+    Pane graphe;
 
     @FXML
     TextField comp1;
@@ -49,8 +56,59 @@ public class ToileController implements Initializable {
     @FXML
     Button boutonVider;
 
+    Circle note1 = new Circle(200,200,5);
+    Circle note2 = new Circle(200,200,5);
+    Circle note3 = new Circle(200,200,5);
+    Circle note4 = new Circle(200,200,5);
+    Circle note5 = new Circle(200,200,5);
+    Circle note6 = new Circle(200,200,5);
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        scene.setBackground(new Background(new BackgroundFill(Color.rgb(145,198,255),null,null)));
+        graphe.getChildren().addAll(note1,note2,note3,note4,note5,note6);
+
+        boutonTracer.setOnAction(actionEvent -> {
+            int posX = 0;
+            int posY = 0;
+
+            // mise à jour du point de la compétence 1
+            posX = getXRadarChart(Double.parseDouble(comp1.getText()),1);
+            posY = getYRadarChart(Double.parseDouble(comp1.getText()),1);
+            note1.setCenterX(posX);
+            note1.setCenterY(posY);
+
+            // mise à jour du point de la compétence 2
+            posX = getXRadarChart(Double.parseDouble(comp2.getText()),2);
+            posY = getYRadarChart(Double.parseDouble(comp2.getText()),2);
+            note2.setCenterX(posX);
+            note2.setCenterY(posY);
+
+            // mise à jour du point de la compétence 3
+            posX = getXRadarChart(Double.parseDouble(comp3.getText()),3);
+            posY = getYRadarChart(Double.parseDouble(comp3.getText()),3);
+            note3.setCenterX(posX);
+            note3.setCenterY(posY);
+
+            // mise à jour du point de la compétence 4
+            posX = getXRadarChart(Double.parseDouble(comp4.getText()),4);
+            posY = getYRadarChart(Double.parseDouble(comp4.getText()),4);
+            note4.setCenterX(posX);
+            note4.setCenterY(posY);
+
+            // mise à jour du point de la compétence 5
+            posX = getXRadarChart(Double.parseDouble(comp5.getText()),5);
+            posY = getYRadarChart(Double.parseDouble(comp5.getText()),5);
+            note5.setCenterX(posX);
+            note5.setCenterY(posY);
+
+            // mise à jour du point de la compétence 6
+            posX = getXRadarChart(Double.parseDouble(comp6.getText()),6);
+            posY = getYRadarChart(Double.parseDouble(comp6.getText()),6);
+            note6.setCenterX(posX);
+            note6.setCenterY(posY);
+        });
     }
 
     int getXRadarChart(double value, int axe ){
